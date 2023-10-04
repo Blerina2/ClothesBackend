46f5effa7471a4b51e5810db045d38442f7648e0
albulenamakolli1 <96053284+albulenamakolli1@users.noreply.github.com>
2023-10-04 22:43:07 +0200

    Created models

---
+++ models/user.entity.js
@@ -0,0 +1,14 @@
+'use strict';
+class UserEntity {
+    id;
+    firstname;
+    lastname;
+    birthday;
+    email
+    password;
+    role;
+    address;
+    phone;
+}
+
+module.exports = UserEntity;
