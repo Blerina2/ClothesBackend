57c71f90b6746e69360f53545b3b95537d339716
albulenamakolli1 <96053284+albulenamakolli1@users.noreply.github.com>
2023-10-05 23:15:20 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const serverConfig = require('./configs/server.config');
+const databaseConfig = require('./configs/database.config');
+const express = require("express"); // https://www.npmjs.com/package/express
+const mongoose = require("mongoose"); // https://www.npmjs.com/package/mongoose
+const bodyParser = require('body-parser'); // https://www.npmjs.com/package/body-parser
+const morgan = require('morgan'); // https://www.npmjs.com/package/morgan
+const app = express();
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./services/error.handler');
+const swaggerUi = require('swagger-ui-express');
+const SwaggerSpec = require('./swagger');
+const basicAuth = require('express-basic-auth');
+
+// swagger: start the Express server (server.js), and navigate to http://localhost:8080/swaggerClothes-docs/ s in the browser.
+app.use("/swaggerClothes-docs",basicAuth({
+    users: {'ibrahim.rugova@president-ksgov.net': 'KosovA1!?_'},
+    challenge: true,
+}), swaggerUi.serve, swaggerUi.setup(SwaggerSpec));
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
